8a318d9923a9114d4fd03121319c6d2c4704291a
shivam <shivam.singhal@codinova.com>
2024-06-27 12:13:43 +0530

    adding context to the server

--- src/server.js
+++ src/server.js
@@ -1,9 +1,15 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { schema } from "./graphQL/schema.js";
+import { createContext } from "./graphQL/createContext.js"
+import { createServices } from "./services/createServices.js"
 
 const server = new ApolloServer({ schema });
+const services = createServices();
 
-startStandaloneServer(server, { listen: { port: 8000 } }).then(({ url }) =>
-  console.log(`server is running at ${url}`)
-);
+startStandaloneServer(server, {
+  listen: { port: 8000 },
+  async context() {
+    return createContext(services);
+  },
+}).then(({ url }) => console.log(`server is running at ${url}`));
